c01928c9fb550923cd483449eec52a63216095d8
dbensaid <driss.bensaid@epfedu.fr>
2022-11-29 15:36:19 +0100

    second commit

--- main.py
+++ main.py
@@ -3,5 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
+
 def hello_world():
-    return "Hello World, welcome on Driss Bensaid website"
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-HSQTDR354L"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ' G-HSQTDR354L');
+    </script>
+    """
+    return prefix_google + "Hello World, welcome on Driss Bensaid website"
